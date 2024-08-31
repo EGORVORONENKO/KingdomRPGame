d2c4a79045121b843826d0135b9f0297b32d579e
EGORVORONENKO <ronnyofficial@vk.com>
2024-08-31 15:07:17 +0600

    UI
    UI

---
+++ KingdomRP/Assets/Script/SceneChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Scenes : MonoBehaviour
+{
+    public void OpenMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OpenGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+	
+	public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
